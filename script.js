0e4e3b8ae66d9310af59d9b4e227d2118c7e717c
Hans Notelteirs <notelteirs@gmail.com>
2022-08-02 06:21:37 +0000

    Info page formatted. Added multiple html pages per floor. photo flow working but not perfect.

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-
 $(function() {
-
-	$(".photo").each(function(index) {
-		var photoTopCordinates = ($(this).offset().top - $(window).scrollTop());
-		if (photoTopCordinates < -10 || photoTopCordinates > 300) {
-			$(this).css('border', '0px solid teal');
-			$(this).css('filter', 'opacity(.5)');
-		} else {
-			$(this).css('border', '6px solid teal');
-			$(this).css('filter', 'opacity(1)');
-
-		}
-	});
-
+	setPhoto();
 	$(window).scroll(function() {
-
-		$(".photo").each(function(index) {
-			var photoTopCordinates = ($(this).offset().top - $(window).scrollTop());
-			if (photoTopCordinates < 0 || photoTopCordinates > 300) {
-				$(this).css('border', '0px solid teal');
-				$(this).css('filter', 'opacity(.5)');
-			} else {
-				$(this).css('border', '6px solid teal');
-				$(this).css('filter', 'opacity(1)');
-			}
-		});
+		setPhoto();
 	});
 });
+
+
+
+console.log(result); 
+
+	
+}
+function setPhoto () {
+
+
+	
+	$(".photo").each(function(index) {
+			$(this).css('border', '0px solid teal');
+			$(this).css('filter', 'opacity(.5)');
+			var firstPhoto = 1;
+			var photoTopCordinates = ($(this).offset().top - $(window).scrollTop());
+			if (photoTopCordinates > 0 && photoTopCordinates < 325) {
+				
+				if(firstPhoto == 1){
+					$(this).css('border', '6px solid teal');
+				$(this).css('filter', 'opacity(1)');
+					var mapImage= $(this).attr("mapImage");
+					$("#map").attr("src","images/maps/"+ mapImage);
+					firstPhoto = 0;
+				}
+				
+
+				
+				
+				
+			} 
+		});
+}
