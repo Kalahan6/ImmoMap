4a4bd49731ae66f9bba16ef27a92f72a3ee5f75d
Hans Notelteirs <notelteirs@gmail.com>
2022-08-03 14:37:30 +0000

    fixed multiple photos being selected on scroll

--- script.js
+++ script.js
@@ -7,33 +7,34 @@ $(function() {
 
 
 
-console.log(result); 
+function setPhoto() {
+	var noPhotoSelected = true;
+	$(".photo").each(function() {
+		$(this).css('border', '0px solid teal');
+		$(this).css('filter', 'opacity(.5)');
+		var firstPhoto = 1;
 
-	
-}
-function setPhoto () {
-
-
-	
-	$(".photo").each(function(index) {
-			$(this).css('border', '0px solid teal');
-			$(this).css('filter', 'opacity(.5)');
-			var firstPhoto = 1;
-			var photoTopCordinates = ($(this).offset().top - $(window).scrollTop());
-			if (photoTopCordinates > 0 && photoTopCordinates < 325) {
-				
-				if(firstPhoto == 1){
-					$(this).css('border', '6px solid teal');
+		var photoTopCordinates = ($(this).offset().top - $(window).scrollTop());
+		if (photoTopCordinates > 0 && photoTopCordinates < (this.height + 30)) {
+			noPhotoSelected = false;
+			if (firstPhoto == 1) {
+				$(this).css('border', '6px solid teal');
 				$(this).css('filter', 'opacity(1)');
-					var mapImage= $(this).attr("mapImage");
-					$("#map").attr("src","images/maps/"+ mapImage);
-					firstPhoto = 0;
-				}
-				
+				var mapImage = $(this).attr("mapImage");
+				$("#map").attr("src", "images/maps/" + mapImage);
+				firstPhoto = 0;
+			} else {
+				$(this).css('border', '0px solid teal');
+				$(this).css('filter', 'opacity(.5)');
+			}
 
-				
-				
-				
-			} 
-		});
+
+
+
+
+		}
+	});
+	if (noPhotoSelected == true) {
+		$("#map").attr("src", "images/maps/map-0.png");
+	}
 }
